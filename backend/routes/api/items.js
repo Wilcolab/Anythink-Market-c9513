d69f9d59bcdefac7a4eb346875d3993831d839cd
GagaXor <93358531+GagaXor@users.noreply.github.com>
2022-07-27 20:37:16 +0100

    Update items.js

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -147,7 +147,7 @@ router.post("/", auth.required, function (req, res, next) {
       }
 
       var item = new Item(req.body.item);
-      item.image = "placeholder.png";
+      if (!item.image) item.image = "placeholder.png";
       item.seller = user;
 
       return item.save().then(function () {
