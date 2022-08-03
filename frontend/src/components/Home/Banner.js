ae6ca73a9238bfff691db97f7e18e9e53281ea52
GagaXor <93358531+GagaXor@users.noreply.github.com>
2022-08-03 13:29:17 +0100

    Update Banner.js

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,6 +1,31 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 
+const SearchBox = () => {
+  const [title, setTitle] = React.useState();
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setTitle(value);
+
+    if (title.length >= 3) {
+      searchItems();
+    }
+  };
+
+  const searchItems = () => {};
+  return (
+    <>
+      <input
+        type="text"
+        className="search-box"
+        onChange={handleChange}
+        value={title}
+      ></input>
+    </>
+  );
+};
+
 const Banner = () => {
   return (
     <div className="banner text-white">
@@ -8,6 +33,7 @@ const Banner = () => {
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <SearchBox></SearchBox>
           <span> the cool stuff.</span>
         </div>
       </div>
